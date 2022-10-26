2213706497db5458cc5610bce28aad061bded1a2
hassan mohamed kamel <hassankamel755@gmail.com>
2022-10-27 01:35:05 +0200

    header completed

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import { ThemeProvider } from 'styled-components';
+import Header from './components/Header';
+import GlobalStyles from './components/styles/Global';
+
+const theme = {
+  colors: {
+    header: '#ebfbff',
+    body: '#fff',
+    footer: '#003333',
+  },
+  mobile: '768px',
+};
+function App() {
+  return (
+    <ThemeProvider theme={theme}>
+      <GlobalStyles />
+      <Header />
+    </ThemeProvider>
+  );
+}
+
+export default App;
